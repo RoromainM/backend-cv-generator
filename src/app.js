faae3dad5c81db04d1551fdf5e6e97b11d9af248
nghS <thnghia84@gmail.com>
2024-11-26 23:06:01 +0100

    💡: suppression commentaires inutiles

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/auth');  // Importer les routes d'authentification
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
-// Middleware pour analyser les requêtes JSON
 app.use(express.json());
 
-// Connexion à la base de données MongoDB
 mongoose
-  .connect(process.env.MONGO_URI)  // Assure-toi que MONGO_URI est bien défini dans ton fichier .env
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ Connection has been established successfully');
   })
@@ -18,15 +16,12 @@ mongoose
     console.error('❌ Unable to connect to the database: ', error);
   });
 
-// Routes pour l'authentification
-app.use('/api/auth', authRoutes);  // Toute requête vers /api/auth sera traitée par les routes auth
+app.use('/api/auth', authRoutes);
 
-// Route de test
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
-// Démarrer le serveur
 app.listen(3000, () => {
   console.log('🚀 Server is running on port 3000');
 });
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,19 +3,15 @@ const router = express.Router();
 const authController = require('../controllers/auth');
 const jwtMiddleware = require('../middleware/jwt');  // Assurez-vous du chemin du middleware
 
-// Route pour l'inscription
 router.post('/register', authController.register);
 
-// Route pour le login
 router.post('/login', authController.login);
 
-// Exemple de route protégée
 router.get('/protected', jwtMiddleware, (req, res) => {
-    // Si le token est valide, la requête arrive ici
     res.status(200).send({
       success: true,
       message: 'Accès autorisé à la ressource protégée.',
-      user: req.user // Cela contient les données de l'utilisateur extraites du token
+      user: req.user
     });
   });
 

ed09f4b17bf7a3fa5fe6491b66a420e377233df3
Othhmane <othmane.haddouche.dz@viacesi.fr>
2024-11-26 21:15:36 +0100

    oth create

--- src/models/user.js
+++ src/models/user.js
@@ -1,30 +1,34 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Schéma pour l'utilisateur
-const userSchema = new Schema({
-  firstname: {
-    type: String,
-    required: true
-  },
-  lastname: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true  // Email doit être unique
-  },
-  password: {
-    type: String,
-    required: true
-  }
-}, {
-  timestamps: true  // Ajoute createdAt et updatedAt automatiquement
-});
+const UserSchema = new Schema(
+    {
+        firstname: {
+            type: String,
+            required: true,
+        },
+        lastname: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        profileVisibility: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
 
-// Création du modèle d'utilisateur basé sur le schéma
-const User = mongoose.model('User', userSchema);
-
+const User = mongoose.model('User', UserSchema);
 module.exports = User;
